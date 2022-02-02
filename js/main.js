f3f9ef65ce42ea87df7ddc4c7bc60a20dc44edd1
AKSHAY MAHAJAN <akshaymahajan@AKSHAYs-MacBook-Pro.local>
2022-02-01 20:52:55 -0500

    List items click working and implemented switch case for buttons

--- js/main.js
+++ js/main.js
@@ -1,87 +1,151 @@
 'use strict'
 import songs from './songs.js';
 let currentSong = 0;
-let player=document.getElementById('player');
+    
 const PLAYER= {
     
+    player:null,
+    
     init: ()=>{
-        PLAYER.addListeners();
+        PLAYER.player=document.getElementById('player');
         PLAYER.buildPlaylist();
-        player.src = songs[currentSong].src; //load the song
+        PLAYER.addListeners();
+        PLAYER.loadSong(true);
     },
 
     addListeners: ()=>{
-        let btnPlay = document.getElementById('btnPlay');
-        btnPlay.addEventListener('click',PLAYER.btnPlay);
-        let btnPause = document.getElementById('btnPause');
-        btnPause.addEventListener('click',PLAYER.btnPause);
-        let btnStop = document.getElementById('btnStop');
-        btnStop.addEventListener('click',PLAYER.btnStop);
-        player.addEventListener('ended', PLAYER.playNextTrack);
-        player.addEventListener('play', PLAYER.startAnimations);
-        player.addEventListener('durationchange', PLAYER.updateTotalTime);
-        player.addEventListener('timeupdate', PLAYER.updateCurrentTime);
+        document.getElementById('controls').addEventListener('click',PLAYER.routing);
+        document.querySelector('.unstyled-list').addEventListener('click',PLAYER.clickedSong);
+        PLAYER.player.addEventListener('ended', PLAYER.playNextTrack);
+        PLAYER.player.addEventListener('play', PLAYER.startAnimations);
+        PLAYER.player.addEventListener('durationchange', PLAYER.updateTotalTime);
+        PLAYER.player.addEventListener('timeupdate', PLAYER.updateCurrentTime);
     },
 
+    loadSong:(firstLoad)=>{
+        if(firstLoad) {
+            PLAYER.player.src = songs[currentSong].src; //load the song
+            PLAYER.albumArt();
+        }else {
+            PLAYER.player.src = songs[currentSong].src; //load the song
+            PLAYER.albumArt();
+            PLAYER.player.play();
+        }
+    },
+    
+    //to choose which function to call based on which button was clicked
+    routing: (ev)=> {
+        let clickedButton = ev.target.closest('.buttons');
+        switch(clickedButton.id) {
+            case "btnPlay":
+            PLAYER.btnPlay();
+            break;
+            
+            case "btnPause":
+            PLAYER.btnPause();
+            break;
+            
+            case "btnStop":
+            PLAYER.btnStop();
+            break;
+        }
+    },
+    //builds the whole playlist from data in the songs file
     buildPlaylist: ()=> {
         let playlistArea = document.getElementById("playlist-area");
         let ul = document.createElement("ul");
         ul.className="unstyled-list";
-        
-        songs.forEach(song => {
+        songs.forEach((song,index) => {
             let li = document.createElement("li");
             li.classList.add("playlist-item");
+            li.setAttribute('data-index',index);
+            //setting the active class on player load to the first song
+            if(index===0){
+                li.classList.add('active')
+            }
             let span = document.createElement("span");
             span.classList.add("thumbnail");
+            
             let img = document.createElement("img");
             img.src=song.img;
             img.alt="cover image";
+            
             let div = document.createElement('div');
             div.className='detail';
+            
             let track = document.createElement("p");
             track.classList.add("song");
             track.textContent=song.title;
+            
             let artist = document.createElement("p");
             artist.classList.add("artist");
             artist.textContent=song.artist;
+            
             div.append(track, artist);
             span.append(img);
             li.append(span,div);
             ul.append(li);
-        })
+        });
         playlistArea.append(ul);
-        PLAYER.albumArt();
     },
 
     albumArt: ()=> {
         let imgWrapper = document.querySelector('.img-wrapper');
-        let albumArt = document.createElement('img');
-        albumArt.src = songs[currentSong].img;
-        albumArt.alt = `${songs[currentSong].title} album art`;
-        imgWrapper.append(albumArt);
+        //check if album art already exists, if not create new else replace it to match the currently playing song
+        if(imgWrapper.firstElementChild) {
+            //check if the displayed album art matches the currently playing song's album art
+            if(imgWrapper.firstElementChild.src !== songs[currentSong].img) {
+                imgWrapper.firstElementChild.src = songs[currentSong].img;
+            }
+        } else {
+            let albumArt = document.createElement('img');
+            albumArt.src = songs[currentSong].img;
+            albumArt.alt = `${songs[currentSong].title} album art`;
+            imgWrapper.append(albumArt);
+        }
     },
 
-    btnPlay: (ev)=> {
-        let clickedButton = ev.currentTarget;
-        document.querySelector('.hidden').classList.remove('hidden');
-        clickedButton.classList.add('hidden');
-        player.play();
+    btnPlay: ()=> {
+        PLAYER.player.play();
+        PLAYER.togglePlayPause();
     },
 
-    btnPause:(ev)=> {
-        player.pause();
-        let clickedButton = ev.currentTarget;
-        document.querySelector('.hidden').classList.remove('hidden');
-        clickedButton.classList.add('hidden');
+    btnPause:()=> {
+        PLAYER.player.pause();
+        PLAYER.togglePlayPause();
+        PLAYER.stopAnimations();
+    },
+
+    togglePlayPause: ()=>{
+        let hiddenButton = document.querySelector('.hidden');
+        hiddenButton.classList.remove('hidden');
+        let id = hiddenButton.getAttribute('id');
+        if(id ==='btnPlay') {
+            document.getElementById('btnPause').classList.add('hidden');
+        }else {
+            document.getElementById('btnPlay').classList.add('hidden');
+        }
     },
 
     btnStop:()=>{
-        player.pause();
-        player.currentTime = 0;
-        let hiddenButton = document.querySelector('.hidden');
-        if(hiddenButton.hasAttribute('id','btnPlay')) {
-            hiddenButton.classList.remove('hidden');
-            document.getElementById('btnPause').classList.add('hidden');
+        PLAYER.player.pause();
+        PLAYER.player.currentTime = 0;
+        PLAYER.togglePlayPause();
+        PLAYER.stopAnimations();
+    },
+
+    clickedSong: (ev)=>{
+        let clickedSong = ev.target.closest('.playlist-item');
+        let index = clickedSong.getAttribute('data-index');
+        if(index !== currentSong.toString()){
+            document.querySelector('.active').classList.remove('active');
+            clickedSong.classList.add('active');
+            currentSong=index;
+            let playerArea = document.getElementById('player-whole');
+            if(playerArea.className==='paused'){
+                PLAYER.togglePlayPause();
+            }
+            PLAYER.loadSong();
         }
     },
 
@@ -92,18 +156,22 @@ const PLAYER= {
     startAnimations: ()=> {
         let playerArea = document.getElementById('player-whole');
         playerArea.className='is-playing';
-        let playlist = document.getElementById('playlist-area');
-        let listSongs = Array.from(playlist.querySelectorAll('.song')); //fetch all the list items with class of song inside them
-        let activeSong = listSongs.filter(element=> element.innerHTML===songs[currentSong].title); //find the currently playing song's element
-        activeSong[0].parentElement.parentElement.classList.add('active');  //add the active class to the element playlist item
+
+
         // Animation code will go here
+    },
+
+    stopAnimations:()=>{
+        let playerArea = document.getElementById('player-whole');
+        playerArea.className='paused';
+        //code to stop the animations shall go here
     },
 
     updateTotalTime: ()=> {
         let timer = document.getElementById('timer');
-        let duration = Math.round(player.duration); //turn total duration(in seconds) into an integer
+        let duration = Math.round(PLAYER.player.duration); //turn total duration(in seconds) into an integer
         let minutes=Math.floor(duration/60).toString().padStart(2,'0'); //minutes part padded with a zero
-        let seconds =Math.floor(duration%60); //seconds part
+        let seconds =Math.floor(duration%60).toString().padEnd(2,'0'); //seconds part
         timer.lastElementChild.innerHTML=`${minutes}:${seconds}`;
     },
 
